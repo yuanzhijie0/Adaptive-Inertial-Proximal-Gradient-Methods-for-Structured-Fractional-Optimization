4dc5726a51ed5bd6f32b0476f04d7f277e2c7a53
yuanzhijie0 <yuanzhijie0@gmail.com>
2026-04-23 21:30:15 +0800

    Add files via upload

---
+++ demo_main.m
@@ -0,0 +1,30 @@
+%% code for FAIPG-D solving sparse signal recovery
+demo_ssrd();
+
+
+
+%% code for FAIPG-Q solving sparse signal recovery
+demo_ssrq();
+
+
+
+
+%% code for FAIPG-D solving normalized cut problem
+demo_ncutd();
+
+
+
+%% code for FAIPG-Q solving normalized cut problem
+demo_ncutq();
+
+
+
+%% code for FAIPG-D solving generalized eigenvalue problem
+demo_eigd();
+
+
+
+%% code for FAIPG-Q solving generalized eigenvalue problem
+demo_eigq();
+
+
